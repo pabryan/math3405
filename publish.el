65f2056393074cee1632b31647d573d74b31a3e2
Paul Bryan <pabryan@gmail.com>
2017-08-29 14:50:45 +1000

    Changed src directory UQ name to lower case.

--- publish.el
+++ publish.el
@@ -1,5 +1,5 @@
 (setq pb/current-project "math3405")
-(setq pb/org-src-path  (concat "~/working/academic/teaching/UQ/2017/" pb/current-project))
+(setq pb/org-src-path  (concat "~/working/academic/teaching/uq/2017/" pb/current-project))
 (setq pb/org-publish-path (concat "~/publish/academic/teaching/uq/2017/" pb/current-project))
 
 (setq org-publish-project-alist (pb/create-org-publish-beamer-alist pb/current-project pb/org-src-path pb/org-publish-path))
